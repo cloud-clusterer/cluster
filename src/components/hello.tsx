cb2b072457c9f0b643826fa7fb5e833f0dc46cee
chrisbarbour <bobjamin@live.co.uk>
2018-03-04 10:58:58 +0000

    initial commit of typescript react project

---
+++ src/components/hello.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export interface HelloProps { compiler: string; framework: string; }
+
+export const Hello = (props: HelloProps) => <h1>Hello kjjk2 from {props.compiler} and {props.framework}!</h1>;
