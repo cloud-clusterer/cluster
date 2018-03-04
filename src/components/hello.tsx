47f85776224c90aa26745b75e3d7c704cdb17e7c
chrisbarbour <bobjamin@live.co.uk>
2018-03-04 11:30:01 +0000

    added webpack dev server

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -2,4 +2,4 @@ import * as React from "react";
 
 export interface HelloProps { compiler: string; framework: string; }
 
-export const Hello = (props: HelloProps) => <h1>Hello kjjk2 from {props.compiler} and {props.framework}!</h1>;
+export const Hello = (props: HelloProps) => <h1>Helllo [kkojlpjk2l from {props.compiler} and {props.framework}!</h1>;
